953aedb430bd5e59a500f833804135a92a284ab7
Abubakr <thefox5000@gmail.com>
2024-01-04 09:04:28 +0300

    Call stack

---
+++ Call Stack/file.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+void Func4 () {
+    cout << "From 4" << endl;
+} 
+
+
+void Func3 () {
+    cout << "From 3" << endl;
+    Func4();
+} 
+
+
+void Func2 () {
+    cout << "From 2" << endl;
+    Func3();
+} 
+
+
+void Func1 () {
+    cout << "From 1" << endl;
+    Func2();
+} 
+
+int main () {
+
+    Func1();
+
+    return 0;
+}

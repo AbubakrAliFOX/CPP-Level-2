1fd465a316a004cce6bcdcf701c2ec22f47633b1
Abubakr <thefox5000@gmail.com>
2024-01-08 08:27:34 +0300

    Two dimensional arrays

---
+++ Two Dimensional Arrays/file.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void MultiplicationTable(int Number)
+{
+    int Table[100][100];
+
+    for (int i = 1; i <= Number; i++)
+    {
+        for (int j = 1; j <= Number; j++)
+        {
+            
+            Table[i-1][j-1] = (i) * (j);
+            
+        }
+    }
+}
+
+void PrintTable(int Number)
+{
+    int Table[100][100];
+
+    for (int i = 0; i < Number; i++)
+    {
+        for (int j = 0; j < Number; j++)
+        {
+            printf("%0*d ", 2, Table[i][j]);
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    MultiplicationTable(10);
+    PrintTable(10);
+
+    return 0;
+}
